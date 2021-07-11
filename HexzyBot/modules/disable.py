255e11e8861a8101977631a6061a3ee90195b9eb
Anki Vector <84716854+ankivector@users.noreply.github.com>
2021-07-11 21:11:47 +0530

    Update disable.py

--- HexzyBot/modules/disable.py
+++ HexzyBot/modules/disable.py
@@ -348,7 +348,7 @@ if is_module_loaded(FILENAME):
     ❍ /listcmds*:* list all possible toggleable commands
     """
 
-    __mod_name__ = "⚡️Disable⚡️"
+    __mod_name__ = "🔌 Disable"
 
 else:
     DisableAbleCommandHandler = CommandHandler
