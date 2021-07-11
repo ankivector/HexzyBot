f76a319a652a95aeca95b165b613e80bf8646544
Anki Vector <84716854+ankivector@users.noreply.github.com>
2021-07-11 20:58:19 +0530

    Update backups.py

--- HexzyBot/modules/backups.py
+++ HexzyBot/modules/backups.py
@@ -367,7 +367,7 @@ def get_chat(chat_id, chat_data):
         return {"status": False, "value": False}
 
 
-__mod_name__ = "⚡️Backup⚡️"
+__mod_name__ = "🧶 Backup"
 
 __help__ = """
 *Only for group owner:*
