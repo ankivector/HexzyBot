91621c1477f3a754d8ee680993e9701ba9add49c
Anki Vector <84716854+ankivector@users.noreply.github.com>
2021-07-11 20:56:31 +0530

    Update antiflood.py

--- HexzyBot/modules/antiflood.py
+++ HexzyBot/modules/antiflood.py
@@ -425,7 +425,7 @@ will result in restricting that user.
  `1w` = 1 week
  """
 
-__mod_name__ = "⚡️Control⚡️"
+__mod_name__ = "🍿 Control"
 
 FLOOD_BAN_HANDLER = MessageHandler(
     Filters.all & ~Filters.status_update & Filters.group, check_flood
