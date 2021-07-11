8317ec6ac6a3b8e18758f26b2fed37dab8c7d473
Anki Vector <84716854+ankivector@users.noreply.github.com>
2021-07-11 21:02:17 +0530

    Update blacklist_stickers.py

--- HexzyBot/modules/blacklist_stickers.py
+++ HexzyBot/modules/blacklist_stickers.py
@@ -494,7 +494,7 @@ def __stats__():
     )
 
 
-__mod_name__ = "⚡️Sticker-Bl⚡️"
+__mod_name__ = "🗳 Sticker-Bl"
 
 BLACKLIST_STICKER_HANDLER = DisableAbleCommandHandler(
     "blsticker", blackliststicker, admin_ok=True
