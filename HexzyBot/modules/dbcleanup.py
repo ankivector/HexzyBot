e849157794dca857d29a8b3fe6f9c08d5e4770c8
Anki Vector <84716854+ankivector@users.noreply.github.com>
2021-07-11 21:10:35 +0530

    Update dbcleanup.py

--- HexzyBot/modules/dbcleanup.py
+++ HexzyBot/modules/dbcleanup.py
@@ -146,5 +146,5 @@ BUTTON_HANDLER = CallbackQueryHandler(callback_button, pattern="db_.*")
 dispatcher.add_handler(DB_CLEANUP_HANDLER)
 dispatcher.add_handler(BUTTON_HANDLER)
 
-__mod_name__ = "⚡️DB-Cleanup⚡️"
+__mod_name__ = "🧮 DB-Cleanup"
 __handlers__ = [DB_CLEANUP_HANDLER, BUTTON_HANDLER]
